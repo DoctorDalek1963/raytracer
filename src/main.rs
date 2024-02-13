253957a37a6a15c22f831c2eb4a6be74f801582d
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-02-13 14:37:55 +0000

    Add command line arguments for config

--- src/main.rs
+++ src/main.rs
@@ -7,40 +7,53 @@ mod ray;
 mod vector;
 
 use self::{ray::Ray, vector::v};
+use clap::Parser;
 use color_eyre::{eyre::Context, Result};
 use image::RgbImage;
 use object::Sphere;
 use rayon::iter::ParallelIterator;
 
-/// The width of the whole image.
-const IMG_WIDTH: u32 = 1920;
+#[derive(clap::Parser)]
+#[command(author, version, about)]
+struct Args {
+    /// The full width of the image.
+    #[arg(long, short, default_value_t = 1920)]
+    width: u32,
 
-/// The height of the whole image.
-const IMG_HEIGHT: u32 = IMG_WIDTH * 9 / 16;
+    /// The full height of the image.
+    #[arg(long, short = 'H', default_value_t = 1080)]
+    height: u32,
+
+    /// The path to the output image file.
+    #[arg(long, short, default_value = "./out.png")]
+    output: String,
+}
 
 fn main() -> Result<()> {
     color_eyre::install()?;
 
+    let args = Args::parse();
+
     let vec_height = v!(0, 2, 0);
-    let vec_width = v!(32. / 9., 0, 0);
+    let vec_width = v!(2. * args.width as f64 / args.height as f64, 0, 0);
     let vec_focal_length = v!(0, 0, 1);
 
     let viewport_top_left = -vec_width / 2. + vec_height / 2. - vec_focal_length;
 
-    let mut img = RgbImage::new(IMG_WIDTH, IMG_HEIGHT);
+    let mut img = RgbImage::new(args.width, args.height);
     let scene = vec![
         Sphere::new(v!(0, 0, -1), 0.5),
         Sphere::new(v!(0, -100.5, -1), 100.),
     ];
 
     img.par_enumerate_pixels_mut().for_each(|(i, j, pixel)| {
-        let x_prop = i as f64 / IMG_WIDTH as f64;
+        let x_prop = i as f64 / args.width as f64;
         debug_assert!(
             (0.0..=1.0).contains(&x_prop),
             "The x proportion must be in [0, 1]: {x_prop}"
         );
 
-        let y_prop = j as f64 / IMG_HEIGHT as f64;
+        let y_prop = j as f64 / args.height as f64;
         debug_assert!(
             (0.0..=1.0).contains(&y_prop),
             "The y proportion must be in [0, 1]: {y_prop}"
@@ -52,7 +65,7 @@ fn main() -> Result<()> {
         *pixel = ray.colour(&scene).into();
     });
 
-    img.save("./out.png")
+    img.save(args.output)
         .wrap_err("When trying to save image buffer")?;
 
     Ok(())

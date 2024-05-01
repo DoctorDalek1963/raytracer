63545859d52cc5ba6d625b4c7e1c48891f8c8a0d
DoctorDalek1963 <dyson.dyson@icloud.com>
2024-05-01 20:32:14 +0100

    Add checks to Nix flake

--- flake.nix
+++ flake.nix
@@ -92,6 +92,20 @@
           };
         };
 
+        checks = {
+          inherit (packages) raytracer;
+
+          clippy = craneLib.cargoClippy (commonArgs
+            // {
+              inherit cargoArtifacts;
+              cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+            });
+
+          fmt = craneLib.cargoFmt {
+            inherit src;
+          };
+        };
+
         packages = rec {
           default = raytracer;
 
